437d688929bef7091dfca3c8b176c6a79bedfecf
marco <marcodecrema@libero.it>
2020-09-20 15:06:08 +0200

    creazione fogli

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  for (i=1; i<=31; i++) {
+
+    var day = {
+      "giorno": i,
+      "mese": "Gennaio"
+    }
+
+// Vado a inserire tutto nel tamplate e lo invio all'html
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+  var contenuto = template(day);
+
+  $(".gg-mm").append(contenuto);
+
+  }
+})
