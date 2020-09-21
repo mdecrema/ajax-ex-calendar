0c259d641a0118a9e1cd514d328ba0a89c461ad8
marco <marcodecrema@libero.it>
2020-09-21 13:02:53 +0200

    avanzamento

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
 
+  var calendarDate = moment(date);
+
+  $.ajax({
+  url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+  data: {
+            "year": 2018,
+            "month": 0
+        },
+  method: "GET",
+  success: function (data, stato) {
+    var data = data.response;
+    console.log(data);
+    // Richiamo la funzione 'datiBrano' e gli passo l'argomento
+    cicloGiorni(data);
+  },
+  error: function (richiesta, stato, errori) {
+    alert("Errore: "+errori);
+  }
+})
+
+
+function cicloGiorni(num) {
   for (i=1; i<=31; i++) {
     if (i<10) {
       i = "0"+i;
+    } else {
+      i = i;
     }
+}
+
     var day = {
       "giorno": i,
       "mese": "Gennaio"
